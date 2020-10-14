24a31777945723726027a00283b7a11e568c360b
Mayya Toporova <from.mayya@gmail.com>
2020-10-14 13:49:57 +0300

    Working solution

---
+++ src/main/scala/calculator/Calculator.scala
@@ -0,0 +1,32 @@
+package calculator
+
+sealed abstract class Expr
+final case class Literal(v: Double) extends Expr
+final case class Ref(name: String) extends Expr
+final case class Plus(a: Expr, b: Expr) extends Expr
+final case class Minus(a: Expr, b: Expr) extends Expr
+final case class Times(a: Expr, b: Expr) extends Expr
+final case class Divide(a: Expr, b: Expr) extends Expr
+
+object Calculator extends CalculatorInterface {
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    for {
+      (variable, expr) <- namedExpressions
+    } yield { (variable -> Signal(eval(expr(), namedExpressions))) }
+  }
+
+  def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
+    expr match {
+      case Literal(v) => v
+      case Ref(name) => eval(getReferenceExpr(name, references), references - name)
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+    }
+  }
+
+  private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]) =
+    references.get(name).fold[Expr](Literal(Double.NaN)){ exprSignal => exprSignal() }
+
+}
